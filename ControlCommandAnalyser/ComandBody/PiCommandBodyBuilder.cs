37980f4345a3f02d2879d32659f575c5b96320df
JuliaZorina <96383396+JuliaZorina@users.noreply.github.com>
2025-10-09 09:34:54 +0400

    ключи в sourseline для команды ПИ

--- ControlCommandAnalyser/ComandBody/PiCommandBodyBuilder.cs
+++ ControlCommandAnalyser/ComandBody/PiCommandBodyBuilder.cs
@@ -1,5 +1,7 @@
 ﻿using ControlCommandAnalyser.Model;
+using ControlCommandAnalyser.Parser;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace ControlCommandAnalyser.ComandBody
 {
@@ -14,12 +16,43 @@ namespace ControlCommandAnalyser.ComandBody
       {
         return newSourseLines;
       }
+      var result = AlgorithmKeyParser.ExtractKeysWithTrailingCommaCheck(newSourseLines.ToString());
+      var strSourseLine = newSourseLines.ToString();
+      foreach (var key in model.AlgorithmKey)
+      {
+        strSourseLine = Regex.Replace(
+        strSourseLine,
+        $@"\b{Regex.Escape(key)}\s*,?",
+        "",
+        RegexOptions.IgnoreCase);
+      }
+      newSourseLines.Clear();
+      newSourseLines.Append(strSourseLine);
       var commandBody = new StringBuilder();
       var siCommand = string.Empty;
       if (pi.SiCommand != null)
       {
         var siBuilder = new SiCommandBodyBuilder();
-        commandBody = siBuilder.Create(pi.SiCommand, commandBody); 
+        commandBody = siBuilder.Create(pi.SiCommand, commandBody);
+
+        var algorithmKey = pi.SiCommand.AlgorithmKey;
+        if (algorithmKey != null)
+        {
+          var algorithmKeysList = algorithmKey.ToList();
+
+          for (int i = 0; i < algorithmKeysList.Count; i++)
+          {
+            if (!string.IsNullOrEmpty(algorithmKeysList[i]) && !string.IsNullOrWhiteSpace(algorithmKeysList[i]) && i < algorithmKeysList.Count - 1)
+            {
+              commandBody.Append($"{algorithmKeysList[i]}, ");
+            }
+            else
+            {
+              commandBody.Append($"{algorithmKeysList[i]} ");
+            }
+          }
+        }
+
       }
       if (pi.VoltageType == VoltageEnum.Type.DCW)
       {
@@ -32,6 +65,23 @@ namespace ControlCommandAnalyser.ComandBody
       if (!string.IsNullOrEmpty(pi.TimeSource))
       {
         commandBody.Append($", {pi.TimeSource}");
+      }
+      var piAlgorithmKey = pi.AlgorithmKey;
+      if (piAlgorithmKey != null)
+      {
+        var piAlgorithmKeysList = piAlgorithmKey.ToList();
+        commandBody.Append($", ");
+        for (int i = 0; i < piAlgorithmKeysList.Count; i++)
+        {
+          if (!string.IsNullOrEmpty(piAlgorithmKeysList[i]) && !string.IsNullOrWhiteSpace(piAlgorithmKeysList[i]) && i < piAlgorithmKeysList.Count - 1)
+          {
+            commandBody.Append($"{piAlgorithmKeysList[i]}, ");
+          }
+          else
+          {
+            commandBody.Append($"{piAlgorithmKeysList[i]} ");
+          }
+        }
       }
 
       return newSourseLines.Append(commandBody.ToString());
