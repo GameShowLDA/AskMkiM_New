071df88c7948ab6bcd0a57fa6e383cc4bf269085
Daniil Lukashin <39368626+GameShowLDA@users.noreply.github.com>
2025-10-15 17:17:13 +0400

    Полностью донастроил светлую тему и перенёс данные по цветам в список ресурсов

--- UI/Theme/ThemeProvider.cs
+++ UI/Theme/ThemeProvider.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Resources;
 using System.Text;
@@ -10,27 +11,18 @@ namespace UI.Theme
 {
   public static class ThemeProvider
   {
-    private static readonly string[] ColorKeys =
-    {
-            "LightPrimaryColor", "PrimaryColor", "SecondaryColor", "ForegroundColor",
-            "ForegroundColor60", "ActiveColor", "ActiveColor80", "IsCheckedColor",
-            "IsCheckedColor80", "RedColor", "GreenColor", "YellowColor", "LightBlueColor"
-        };
 
-    /// <summary>
-    /// Загружает все цвета из указанного ресурса и возвращает их как словарь.
-    /// </summary>
     public static Dictionary<string, Color> LoadColors(ResourceManager resourceManager)
     {
       var colors = new Dictionary<string, Color>();
+      var resourceSet = resourceManager.GetResourceSet(CultureInfo.CurrentUICulture, true, true);
 
-      foreach (var key in ColorKeys)
+      foreach (System.Collections.DictionaryEntry entry in resourceSet)
       {
-        string hex = resourceManager.GetString(key);
-        if (string.IsNullOrWhiteSpace(hex))
-          continue;
-
-        colors[key] = (Color)ColorConverter.ConvertFromString(hex);
+        if (entry.Value is string hex && !string.IsNullOrWhiteSpace(hex))
+        {
+          colors[entry.Key.ToString()] = (Color)ColorConverter.ConvertFromString(hex);
+        }
       }
 
       return colors;
