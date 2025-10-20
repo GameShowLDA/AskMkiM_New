2c5563a8dcde602c7d284594e6b858960f78d1fa
Daniil Lukashin <39368626+GameShowLDA@users.noreply.github.com>
2025-10-20 09:49:46 +0400

    Слили актуальные коммиты

--- DataBaseConfigruration/DataBaseConfig.cs
+++ DataBaseConfigruration/DataBaseConfig.cs
@@ -17,12 +17,6 @@ namespace DataBaseConfiguration
       {
         string baseDir = AppContext.BaseDirectory;
         string path = Path.Combine(baseDir, "Resources", "_config.db");
-
-        if (!File.Exists(path))
-        {
-          throw new FileNotFoundException($"Файл базы данных не найден: {path}");
-        }
-
         return path;
       }
     }
@@ -48,7 +42,6 @@ namespace DataBaseConfiguration
       try
       {
         using var context = new AppDbContext(OptionsBuilder.Options);
-        await context.Database.MigrateAsync();
 
         FileHotkeySeeder.Seed(context);
         MeasurementErrorSeeder.Seed(context);
--- UI/Components/ProtocolListBox/HeaderToBrushIfNoMessageConverter.cs
+++ UI/Components/ProtocolListBox/HeaderToBrushIfNoMessageConverter.cs
@@ -24,7 +24,7 @@ namespace UI.Components.ProtocolListBox
           return SuccessBrush;
         }
 
-        return (Color)Application.Current.Resources["tests.protocol.message.header.foreground"];
+        return new SolidColorBrush((Color)Application.Current.Resources["tests.protocol.message.header.foreground"]);
       }
       return headerColor ?? new SolidColorBrush(Colors.White); // Цвет по умолчанию
     }
