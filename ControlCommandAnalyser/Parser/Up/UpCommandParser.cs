e55cb2c7b17cf372a9f0aa10d670d928427583dc
Daniil Lukashin <39368626+GameShowLDA@users.noreply.github.com>
2025-10-30 09:27:18 +0400

    Убрал хуй

--- ControlCommandAnalyser/Parser/Up/UpCommandParser.cs
+++ ControlCommandAnalyser/Parser/Up/UpCommandParser.cs
@@ -33,7 +33,7 @@ namespace ControlCommandAnalyser.Parser.Up
         CommandNumber = commandNumber,
         StartLineNumber = numberLine,
         SourceLines = new List<string>(lines),
-        TargetLabel = targetLabel хуй
+        TargetLabel = targetLabel
       };
 
       List<string> processedLines = CommentsParser.ParseComments(lines, model);
