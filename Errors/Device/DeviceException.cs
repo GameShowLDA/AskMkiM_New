6723446ba3417e1c5004d0512ad564a55a86d3ec
Daniil Lukashin <39368626+GameShowLDA@users.noreply.github.com>
2025-10-30 09:51:52 +0400

    Добавил само событие на исключения оборудования

--- Errors/Device/DeviceException.cs
+++ Errors/Device/DeviceException.cs
@@ -1,28 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Errors.Device
 {
   /// <summary>
   /// Базовое исключение, возникающее при ошибках, связанных с оборудованием.
   /// Используется для всех режимов и компонентов устройств (IR, DCW, PR и др.).
+  /// При создании исключения автоматически вызывает глобальное уведомление.
   /// </summary>
   public class DeviceException : Exception
   {
     /// <summary>
+    /// Глобальное событие, возникающее при создании исключения устройства.
+    /// </summary>
+    public static event Action<DeviceException>? OnDeviceExceptionRaised;
+
+    /// <summary>
     /// Создаёт новое исключение устройства с заданным сообщением.
     /// </summary>
     /// <param name="message">Описание ошибки.</param>
-    public DeviceException(string message) : base(message) { }
+    public DeviceException(string message) : base(message)
+    {
+      Notify(this);
+    }
 
     /// <summary>
     /// Создаёт новое исключение устройства с сообщением и вложенным исключением.
     /// </summary>
     /// <param name="message">Описание ошибки.</param>
     /// <param name="innerException">Внутреннее исключение.</param>
-    public DeviceException(string message, Exception innerException) : base(message, innerException) { }
+    public DeviceException(string message, Exception innerException)
+      : base(message, innerException)
+    {
+      Notify(this);
+    }
+
+    /// <summary>
+    /// Уведомляет всех подписчиков о возникновении исключения.
+    /// </summary>
+    /// <param name="exception">Экземпляр возникшего исключения.</param>
+    private static void Notify(DeviceException exception)
+    {
+      try
+      {
+        OnDeviceExceptionRaised?.Invoke(exception);
+      }
+      catch
+      {
+
+      }
+    }
   }
 }
