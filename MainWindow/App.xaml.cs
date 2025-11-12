469dc4d8d0c5de227b7c4afa2253a29fcbf75734
Лукашин Даниил <lukashin.danilka@mail.ru>
2025-11-12 12:43:19 +0400

    Починил фокус (#92)
    Перенёс активацию под конец загрузки
    
    Co-authored-by: Daniil Lukashin <39368626+GameShowLDA@users.noreply.github.com>
    Reviewed-on: http://192.168.1.91:3000/game_show_lda/AskMkiM/pulls/92

--- MainWindow/App.xaml.cs
+++ MainWindow/App.xaml.cs
@@ -80,8 +80,7 @@ namespace MainWindowProgram
         Application.Current.MainWindow = mainWindow;
 
         mainWindow.Topmost = true;
-        mainWindow.Activate();
-        mainWindow.Focus();
+
 
         await mainWindow.Dispatcher.BeginInvoke(new Action(() =>
         {
@@ -93,6 +92,9 @@ namespace MainWindowProgram
         {
           SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS);
         };
+
+        mainWindow.Activate();
+        mainWindow.Focus();
       }
       catch (Exception ex)
       {
