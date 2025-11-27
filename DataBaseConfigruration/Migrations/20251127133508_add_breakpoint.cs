8815ab09ceb3b68ac4ccdf9792a1e61501ef9ada
Daniil Lukashin <39368626+GameShowLDA@users.noreply.github.com>
2025-11-27 17:38:54 +0400

    Update database

---
+++ DataBaseConfigruration/Migrations/20251127133508_add_breakpoint.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataBaseConfiguration.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_breakpoint : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "BreakpointsMode",
+                table: "Execution",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BreakpointsMode",
+                table: "Execution");
+        }
+    }
+}
