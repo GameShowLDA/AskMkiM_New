0a280ac16230d2051ad33bf1e5bde062b562b3d6
julia_zorina <yulia.a.zzo@gmail.com>
2025-11-27 09:26:11 +0400

    #169 Исправила установку значений времени и напряжения (#178)
    Co-authored-by: JuliaZorina <96383396+JuliaZorina@users.noreply.github.com>
    Co-authored-by: Лукашин Даниил <lukashin.danilka@mail.ru>
    Reviewed-on: http://192.168.1.91:3000/game_show_lda/AskMkiM/pulls/178
    Co-authored-by: julia_zorina <yulia.a.zzo@gmail.com>
    Co-committed-by: julia_zorina <yulia.a.zzo@gmail.com>

--- ControlCommandAnalyser/Parser/Pi/PiCommandParser.cs
+++ ControlCommandAnalyser/Parser/Pi/PiCommandParser.cs
@@ -37,9 +37,9 @@ namespace ControlCommandAnalyser.Parser.Pi
       }
       else
       {
-        var maxDCWVoltage = Measurement.MeasurementTypeCommand.PI_DCW.GetDisplayInfo().UpperLimit; ;// постоянный ток
-        var minVoltage = Measurement.MeasurementTypeCommand.PI_ACW.GetDisplayInfo().LowerLimit; ;
-        var maxACWVoltage = Measurement.MeasurementTypeCommand.PI_ACW.GetDisplayInfo().UpperLimit; ; // переменный ток
+        var maxDCWVoltage = Measurement.MeasurementTypeCommand.PI_DCW.GetDisplayInfo().UpperLimit; // постоянный ток
+        var minVoltage = Measurement.MeasurementTypeCommand.PI_ACW.GetDisplayInfo().LowerLimit;
+        var maxACWVoltage = Measurement.MeasurementTypeCommand.PI_ACW.GetDisplayInfo().UpperLimit; // переменный ток
 
         var rmCommandModel = CommandsModel.GetRMModel();
 
@@ -76,13 +76,14 @@ namespace ControlCommandAnalyser.Parser.Pi
         var remainder = head.Success ? head.Groups[1].Value : body;
 
         LoggerUtility.LogDebug($"Хвост после ПИ: \"{remainder}\"");
-        var (siPart, piPart, errs) = PiSiSplitter.SplitSiFromPiStrict(remainder);
+        var (siPart, piPart, errs) = PiSiSplitter.SplitSiFromPiStrict(body);
         if (errs.Count > 0)
         {
           LoggerUtility.LogWarning($"Strict WS issues: {string.Join(" | ", errs)}");
         }
 
         var modelSi = new SiCommandModel();
+        modelSi.SourceLines = new List<string> { siPart };
         var siRemainder = SiCommandParser.ManageSiParametersParse(modelSi, commandNumber, mnemonic, numberLine, siPart, breakDown);
 
         // Если СИ что-то не допарсила, логни отдельно (в модель СИ, не ПИ)
@@ -98,7 +99,6 @@ namespace ControlCommandAnalyser.Parser.Pi
         {
           model.Errors.AddRange(modelSi.Errors);
         }
-
 
         var remainderPi = piPart;
 
@@ -199,20 +199,22 @@ namespace ControlCommandAnalyser.Parser.Pi
         }
         else
         {
-          LoggerUtility.LogError($"В команде ПИ не указано напряжение.");
-          model.Errors.Add(PiErrors.EmptyVoltage(numberLine, $"{commandNumber} {mnemonic}"));
+          model.Voltage = minVoltage;
+          model.VoltageSource = model.Voltage.Value.ToString() + "В";
+          LoggerUtility.LogDebug($"В команде ПИ не указано напряжение. Установлено значение по умолчанию {minVoltage} В.");
+          //model.Errors.Add(PiErrors.EmptyVoltage(numberLine, $"{commandNumber} {mnemonic}"));
         }
 
         model.Time = string.IsNullOrEmpty(time) || time == null ? 1 : CommonParameterParser.ParseToDouble(time);
         model.TimeSource = string.IsNullOrEmpty(time) || time == null ? "1c" : time + unitTime;
 
-        if (string.IsNullOrWhiteSpace(voltage))
+        if (model.Voltage == null)
         {
           model.Errors.Add(PiErrors.CannotParseParameters("Не указано напряжение", numberLine, $"{commandNumber} {mnemonic}"));
           LoggerUtility.LogWarning($"Не указано напряжение (строка {numberLine}): {commandNumber} {mnemonic}");
         }
 
-        if (string.IsNullOrWhiteSpace(time))
+        if (model.Time == null)
         {
           model.Errors.Add(PiErrors.CannotParseParameters("Не указано время", numberLine, $"{commandNumber} {mnemonic}"));
           LoggerUtility.LogWarning($"Не указано время (строка {numberLine}): {commandNumber} {mnemonic}");
